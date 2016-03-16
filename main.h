ac312f062e2d941449180bb9359df0f5e2c2f42a
Pyhro36 <pllefebvre@if501-219-20.insa-lyon.fr>
2016-03-16 15:25:48 +0100

    modeles

--- main.h
+++ main.h
@@ -2,19 +2,22 @@
                            Parking  -  description
                              -------------------
     début                : 16/03/2016
-    copyright            : (C) par B3129
+    copyright            : (C)2016 par B3129
     e-mail               : pierre-louis.lefebvre@insa-lyon.fr
     					   nicolas.six@insa-lyon.fr
 *************************************************************************/
 
 
-//---------- Interface du module <XXX> (fichier XXX.h) ---------
-#if ! defined ( XXX_H )
-#define XXX_H
+//---------- Interface du module <Main> (fichier main.h) ---------
+#if ! defined ( MAIN_H )
+#define MAIN_H
 
 //------------------------------------------------------------------------
-// Rôle du module <XXX>
-//
+// Rôle du module <Main>
+// Module contenant la methode principale main( ) lancee au debut de
+// l'application et contenant donc la tache mere initialiant tous les
+// elements necessaires au fonctionnement de l'application au debut de
+// celle-ci et les detruisant a la fin celle-ci
 //
 //------------------------------------------------------------------------
 
@@ -27,11 +30,11 @@
 
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
-// type Nom ( liste de paramètres );
+int main();
 // Mode d'emploi :
-//
+//	methode lancee automatiquement au lancement de l'application
 // Contrat :
-//
+//	Aucun
 
-#endif // XXX_H
+#endif // MAIN_H
 
