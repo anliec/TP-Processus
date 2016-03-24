66d8ce850c47f46010181135f8597e16fc6b2b06
Pyhro36 <pylou_11@hotmail.fr>
2016-03-24 20:44:06 +0100

    modif à faire dans Entree.cpp

--- Entree.cpp
+++ Entree.cpp
@@ -36,12 +36,15 @@ static int semId;
 static int *mpPlaceDispo;
 static Voiture *mpParking;
 //---------------------------------------------------- Fonctions internes
+static void init();
+static void initId();
+
 static void init()
 {
     //catch signals
     struct sigaction sigactionUSR, sigactionCHLD;
-    sigactionCHLD.sa_sigaction = &sigChldHandler;
-    sigactionUSR.sa_sigaction = &sigUsr2Handler;
+    sigactionCHLD.sa_handler = &sigChldHandler;
+    sigactionUSR.sa_handler = &sigUsr2Handler;
     sigaction(SIGCHLD, &sigactionCHLD, NULL);
     sigaction(SIGUSR2, &sigactionUSR, NULL);
     //initialistion des ID des resources partagées
@@ -58,9 +61,9 @@ static void initId()
     {
         std::cerr << "unable to get key for msgbuf on " << PATH_TO_MSGBUF << std::endl;
     }
-    else if(msgbuffId = msgget(keyMsgBuf,0660) <0)
+    else if(msgbuffId = msgget(keyMsgBuf,DROITS_ACCES) <0)
     {
-        std::cerr << "unable to open msgbuff on Sortie" << std::endl;
+        std::cerr << "unable to open msgbuf on Entree" << std::endl;
     }
     //récupération de la mémoire partager pour le nombre de place dispo
     key_t keyMpPD = ftok(PATH_TO_MP_PLACEDISPO,PROJECT_ID);
@@ -68,7 +71,7 @@ static void initId()
     {
         std::cerr << "unable to get key for MP on " << PATH_TO_MP_PLACEDISPO << std::endl;
     }
-    else if(mpPlaceDispoId=shmget(keyMpPD,0,0660) <0)
+    else if(mpPlaceDispoId=shmget(keyMpPD,0,DROITS_ACCES) <0)
     {
         std::cerr << "unable to open MP PD on Sortie" << std::endl;
     }
@@ -78,7 +81,7 @@ static void initId()
     {
         std::cerr << "unable to get key for MP on " << PATH_TO_MP_PARKING << std::endl;
     }
-    else if(mpParkingId=shmget(keyMpP,0,0660) <0)
+    else if(mpParkingId=shmget(keyMpP,0,DROITS_ACCES) <0)
     {
         std::cerr << "unable to open MP Parking on Sortie" << std::endl;
     }
@@ -86,7 +89,7 @@ static void initId()
     key_t keySem = ftok(PATH_TO_SEM,PROJECT_ID);
     if(keySem<0)
         std::cerr << "unable to get key for semaphores on Sortie" << std::endl;
-    else if(semId=semget(keySem,NUMBER_OF_SEM,0660) <0)
+    else if(semId=semget(keySem,NUMBER_OF_SEM,DROITS_ACCES) <0)
         std::cerr << "unable to open semaphores on Sortie" << std::endl;
 }
 
@@ -102,12 +105,12 @@ static void attachSharedMemory()
     }
 }
 
-static void sigChldHandler(int signum,siginfo_t *siginfo,void* ucontext)
+static void sigChldHandler(int noSig)
 {
 
 }
 
-static void sigUsr2Handler(int signum,siginfo_t *siginfo,void* ucontext)
+static void sigUsr2Handler(int noSig)
 {
 
 }
@@ -136,6 +139,35 @@ void semV(unsigned short int sem_num)
 
 void Entree(TypeBarriere typeBarriere)
 {
+	//definition des id d'acces au boites aux lettres et semaphores
+	int sgBufEntreeId = MSGBUF_ID_ENTREE_P;
+	int msgbufRequeteId = MSGBUF_ID_REQUETE_P;
+	int msgbufRequeteId = MSGBUF_ID_REQUETE_P;
+	switch (typeBarriere)
+	{
+		case PROF_BLAISE_PASCAL :
+			//deja initialises aux bonnes valeurs;
+			break;
+			
+		case AUTRE_BLAISE_PASCAL :
+		
+			msgBufEntreeId = MSGBUF_ID_ENTREE_A;
+			msgbufRequeteId = MSGBUF_ID_REQUETE_A;
+			semElementSyncEntree = SEMELM_SINC_ENTREE_A;
+			break;
+
+		case ENTREE_GASTON_BERGER :
+		
+			msgBufEntreeId = MSGBUF_ID_ENTREE_GB;
+			msgbufRequeteId = MSGBUF_ID_REQUETE_GB;
+			semElementSyncEntree = SEMELM_SINC_ENTREE_GB;
+			break;
+
+		default :
+
+			std::cerr << "not an entry process error" << std::endl;
+			break;
+	}
     //initialisation
     init();
 
@@ -143,10 +175,17 @@ void Entree(TypeBarriere typeBarriere)
     std::vector<pid_t> listeVoiturier;
     while(1)
     {
-        Commande message;
-        //appel bloquant, attente d'une demande de sortie
-        msgrcv(msgbuffId,&message, sizeof(Commande),MSGBUF_ID_SORTIE,0);
-
+        Voiture voiture;
+        //appel bloquant, attente d'une demande d'entree
+        msgrcv(msgbuffId,&voiture, sizeof(Voiture),msgBufEntreeId,0);
+		if(mpPlaceDispo[0] > 0)
+		{
+		
+		}
+		else
+		{
+		
+		}
         //lance voiturier
         pid_t voiturier = SortirVoiture(message.valeur);
         if(voiturier == -1) //s'il n'y a pas de voiture à cette place on attend une nouvelle demande
